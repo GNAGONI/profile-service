6ec7431dd6cb331303c2f2832e4bca452c38b90e
GNAGONI <nikichetotam@gmail.com>
2020-12-05 15:38:25 -0800

    Add basic configs

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es6: true,
+    node: true,
+    mocha: true,
+  },
+  extends: ['airbnb-base'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'arrow-parens': 0,
+    'no-underscore-dangle': 0,
+    'no-console': 0,
+    'linebreak-style': 0,
+    'no-unused-vars': 0,
+  },
+};

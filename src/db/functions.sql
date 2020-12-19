1d48d62c9fdde8b71d3caf1992358d849b8dcf3b
GNAGONI <nikichetotam@gmail.com>
2020-12-19 08:43:43 -0800

    Refactor postgres functions

--- src/db/functions.sql
+++ src/db/functions.sql
@@ -1,14 +1,37 @@
 DROP FUNCTION IF EXISTS get_profile_by_id;
 CREATE OR REPLACE FUNCTION get_profile_by_id(profile_id uuid)
- 		RETURNS SETOF profiles AS $$
- 	SELECT * FROM profiles
- 	WHERE id = profile_id;
-$$ LANGUAGE SQL;
+RETURNS SETOF profiles AS $$
+DECLARE
+	profile_check BOOLEAN;	
+BEGIN
+  	SELECT EXISTS(
+ 			SELECT * FROM profiles
+  		WHERE id = profile_id
+  	) INTO profile_check;
+	
+ 	IF (profile_check = false) THEN
+ 		RAISE EXCEPTION 'Profile with provided id does not exist'; 
+ 	END IF;
+	
+   	RETURN QUERY SELECT * FROM profiles
+   	WHERE id = profile_id;
+END;
+$$ LANGUAGE plpgsql;
 
 DROP FUNCTION IF EXISTS delete_profile_by_id;
-DROP FUNCTION IF EXISTS delete_profile_by_id;
 CREATE OR REPLACE FUNCTION delete_profile_by_id(profile_id uuid) RETURNS SETOF profiles AS $$
+DECLARE
+	profile_check BOOLEAN;
 BEGIN
+	 SELECT EXISTS(
+ 		SELECT * FROM profiles
+  		WHERE id = profile_id
+  	) INTO profile_check;
+	
+	IF (profile_check = false) THEN
+ 		RAISE EXCEPTION 'Profile with provided id does not exist'; 
+ 	END IF;
+	
 	RETURN QUERY
 		WITH dp as (DELETE FROM profiles WHERE id = profile_id RETURNING *)
   	SELECT * FROM dp;
